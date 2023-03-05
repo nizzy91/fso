9a4a368617ece537e2f4b3c1a715fc031e34d7f5
nizzy <nizzy.cs@gmail.com>
2023-03-05 08:29:06 +0100

    part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,52 @@
-const App = () => (
-   <div>
-      <p>Hello world</p>
-   </div>
-);
+const Header = (props) => {
+   return (
+      <>
+         <h1>{props.course}</h1>
+      </>
+   );
+};
 
+const Part = (props) => {
+   return (
+      <p>
+         {props.partName} {props.exercisesCount}
+      </p>
+   );
+};
+
+const Content = (props) => {
+   console.log(props);
+   return (
+      <div>
+         <Part
+            partName={props.parts[0].name}
+            exercisesCount={props.parts[0].exercises}
+         />
+      </div>
+   );
+};
+
+const App = () => {
+   const course = "Half Stack application development";
+   const parts = [
+      {
+         name: "Fundamentals of React",
+         exercises: 10,
+      },
+      {
+         name: "Using props to pass data",
+         exercises: 7,
+      },
+      {
+         name: "State of a component",
+         exercises: 14,
+      },
+   ];
+   return (
+      <div>
+         <Header course={course} />
+         <Content parts={parts} />
+      </div>
+   );
+};
 export default App;
